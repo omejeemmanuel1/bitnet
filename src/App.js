c29cacf137c985c03c1ec9cb1244642583421602
Omeje Emmanuel Xavier <omejeemmanuel046@yahoo.com>
2022-01-24 09:19:29 +0100

    Bitnet explore

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import react from 'react';
 import './App.css';
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Product from './components/pages/Product';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+             <Navbar />
+         <Routes>
+           <Route path = '/' exact element={<Home />} />
+           <Route path = '/services'  element={<Services />} />
+           <Route path = '/products'  element={<Product />} />
+           <Route path = '/sign-up'  element={<SignUp />} />
+         </Routes>
+      </Router>
+    </>
   );
 }
 
